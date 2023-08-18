fc244862bd6a1523e719e10797f1cd63fd21cd3c
siddharthdb <siddharthdb@gmail.com>
2023-08-17 21:16:58 -0400

    added lodash to get max value for color change

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import "./styles/index.less";
+import { max } from "lodash";
 import * as d3 from "d3";
 import data from "./data.json";
 
 function ExpenseChart() {
-  this.init = () => {
-    let HEIGHT = 178,
-      WIDTH = 460,
-      margin = { top: 30, right: 0, bottom: 30, left: 10 };
+
+  this.init = () => {    
+    const isMobile = window.innerWidth < 575;
+
+    const maxValue = max(data.map(d => d.amount));
+    
+    const HEIGHT = 178,
+      WIDTH = d3.select("#expense-bar").node().getBoundingClientRect().width,
+      margin = { top: 30, right: 0, bottom: 30, left: 0 };
 
     let svg = d3
       .select("#expense-bar")
       .append("svg")
-      .attr("width", d3.select("#expense-bar").node().clientWidth)
+      .attr("width", d3.select("#expense-bar").node().getBoundingClientRect().width)
       .attr("height", HEIGHT + margin.top + margin.bottom)
       .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    
-
-    let x = d3.scaleBand().range([0, WIDTH]).padding(0.2);
+    let x = d3.scaleBand().range([0, WIDTH]).padding(0.28);
     let y = d3.scaleLinear().range([HEIGHT, 0]);
 
     function createAxisLeft(data) {
@@ -56,17 +60,22 @@ function ExpenseChart() {
     }
 
     function addLabelsToBars(bars) {
+
+      const xRectDiff = !isMobile ? 5 : 7;
+      const yRectDiff = !isMobile ? 35 : 30;
+
       bars
         .append("rect")
         .attr("class", "bglbl")
-        .attr("x", (d) => x(d.day) - 5)
-        .attr("y", (d) => y(d.amount) - 35)
+        .attr("x", (d) => x(d.day) - xRectDiff)
+        .attr("y", (d) => y(d.amount) - yRectDiff)
         .attr("rx", 5)
         .attr("ry", 5)
-        .attr("width", 60)
-        .attr("height", 30)
+        .attr("width", isMobile ? 45 : 60)
+        .attr("height", isMobile ? 25 : 30)
         .style("visibility", "hidden")
         .style("fill", "#382314");
+
       bars
         .append("text")
         .text((d) => `$${d.amount}`)
@@ -83,7 +92,7 @@ function ExpenseChart() {
     }
 
     function createBars(data) {
-      let bars = svg
+      const bars = svg
         .selectAll(".bars")
         .data(data, (d) => d.day)
         .enter()
@@ -101,7 +110,14 @@ function ExpenseChart() {
         .attr("ry", 5)
         .attr("width", x.bandwidth())
         .attr("height", 0)
-        .style("fill", "#EC755D")
+        .style("fill", function(d) {
+          if (d.amount === maxValue) {
+            return "#76B5BC";
+          } else {
+            return "#EC755D";
+          }
+
+        })
         .on("mouseover", function(d) {
           d3.select(this.parentNode).select('.txtlbl').style('visibility', 'visible');
           d3.select(this.parentNode).select('.bglbl').style('visibility', 'visible');
@@ -122,9 +138,7 @@ function ExpenseChart() {
 
     createAxisLeft(data);
     createAxisBottom(data);
-    createBars(data);
-
-    
+    createBars(data);    
   };
 }
 
